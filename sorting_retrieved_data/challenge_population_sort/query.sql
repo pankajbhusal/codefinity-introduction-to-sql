cd450912f8dc668cc45ccd9ad4e78a33616acfc5
pankajbhusal <>
2026-04-20 15:06:26 +0000

    Challenge: Population Sort

---
+++ sorting_retrieved_data/challenge_population_sort/query.sql
@@ -0,0 +1,10 @@
+ /*Task
+
+Write an SQL query to retrieve the data from the population column
+using a SELECT statement and sort it in ascending order. Use the 
+ORDER BY clause to sort the results. Replace any placeholders with
+the correct SQL keywords and table/column names.   */
+
+SELECT population
+FROM country
+ORDER BY population
