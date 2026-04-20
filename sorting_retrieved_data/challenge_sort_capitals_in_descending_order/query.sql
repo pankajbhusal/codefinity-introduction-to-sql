b6f7db6f3b55e0bfd459fbf67d4dd9350e236186
pankajbhusal <>
2026-04-20 15:25:34 +0000

    Challenge: Sort Capitals in Descending Order

---
+++ sorting_retrieved_data/challenge_sort_capitals_in_descending_order/query.sql
@@ -0,0 +1,6 @@
+/* Write an SQL query to retrieve all capital values from the country 
+table, and display the  results sorted in descending order.
+*/
+SELECT capital
+From country
+order by capital desc;
