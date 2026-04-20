063f27e126c0de218109b44abac7e44cab972fc9
pankajbhusal <>
2026-04-20 15:11:27 +0000

    Challenge: Dual-Sort Query

---
+++ sorting_retrieved_data/challenge_dual_sort_query/query.sql
@@ -0,0 +1,8 @@
+/* Task
+Write an SQL query to retrieve two columns using the 
+SELECT statement, specifically: region and capital. Sort the 
+result first by region and then by capital.  */
+
+SELECT region, capital
+FROM country
+ORDER By region, capital;
