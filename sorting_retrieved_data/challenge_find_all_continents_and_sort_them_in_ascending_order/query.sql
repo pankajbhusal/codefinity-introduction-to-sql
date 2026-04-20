98619aca4e86d82ff7e68bb5c38dc2c928b3fc50
pankajbhusal <>
2026-04-20 15:31:37 +0000

    Challenge: Find All Continents and Sort Them in Ascending Order

---
+++ sorting_retrieved_data/challenge_find_all_continents_and_sort_them_in_ascending_order/query.sql
@@ -0,0 +1,4 @@
+-- Write an SQL query to get unique continent values from the 
+-- country table, and display the result in ascending order.
+SELECT DISTINCT continent from country
+ORDER BY continent;
