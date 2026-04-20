b938e3b5ce7c35773c53b85e3841b3437ec57d11
pankajbhusal <>
2026-04-20 15:00:26 +0000

    Challenge: Find Country Capitals

---
+++ retrieving_data/challenge_find_country_capitals/query.sql
@@ -0,0 +1,9 @@
+-- Write your SQL query below to get unique capital values from the country table
+
+-- Write an SQL query to retrieve all unique values in the capital 
+-- column from the country table. Use the SELECT statement with the 
+-- DISTINCT keyword to ensure that each capital appears only once in 
+-- the result. Your query should select only the capital column.
+
+SELECT DISTINCT capital from country;
+
